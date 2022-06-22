1d75247ee06f61db8d69d990b7e7a0280a081cc5
Caio Macedo <caiopierre7@hotmail.com>
2022-06-22 00:10:25 -0300

    Add responsive table

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,8 @@
 import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
-import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 import { ApexOptions } from 'apexcharts';
+import { Header } from '../components/Header';
 
 const Chart = dynamic(() => import('react-apexcharts'), {
 	ssr: false,
@@ -78,13 +78,13 @@ export default function Dashboard() {
 					minChildWidth="320px"
 					alignItems="flex-start"
 				>
-					<Box p="8" bg="gray.800" borderRadius={8} pb="4">
+					<Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
 						<Text fontSize="lg" mb="4">
 							Inscritos da semana
 						</Text>
 						<Chart options={options} series={series} type="area" height={160} />
 					</Box>
-					<Box p="8" bg="gray.800" borderRadius={8} pb="4">
+					<Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
 						<Text fontSize="lg" mb="4">
 							Taxa de abertura
 						</Text>
